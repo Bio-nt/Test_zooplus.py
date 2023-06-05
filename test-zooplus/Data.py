298f7002a8bc06c14e9ac17086219e5891c5afa9
piimb <pi.imbor@gmail.com>
2023-06-05 20:01:45 +0200

    Test_Zooplus v1.0

---
+++ test-zooplus/Data.py
@@ -0,0 +1,47 @@
+# Page Object Model
+import datetime
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+class Data:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def open(self):
+        self.driver.get('https://www.zooplus.pl/')
+
+    def click_cookies(self):
+        button = self.driver.find_element('id', 'onetrust-accept-btn-handler')
+        button.click()
+
+    def make_screenshot(self):
+        now = datetime.datetime.now()
+        screenshot = 'screenshot' + now.strftime('_%H%M%S') + '.png'
+        self.driver.get_screenshot_as_file(screenshot)
+
+    def enter_product(self):
+        field = self.driver.find_element('id', 'search_query_field_desktop')
+        field.clear()
+        field.send_keys('mokra karma dla kota')
+        field.send_keys(Keys.ENTER)
+
+    def enter_to_cart(self):
+        overview = self.driver.find_element(By.CLASS_NAME, 'cartButton-module_cartButton__lIc-O')
+        overview.click()
+
+    def delete_product(self, productquantity):
+        cart2 = self.driver.find_element(By.CSS_SELECTOR, 'button[data-zta="reduceQuantityBtn"]')
+        for _ in range(productquantity):
+            cart2.click()
+
+    def choosing_product(self, productnumber):
+        elements = self.driver.find_elements(By.CSS_SELECTOR, 'a[data-zta="product-link"]')
+        elements[productnumber].click()
+
+    def add_product(self, producttype, productquantity):
+        addproduct = self.driver.find_elements(By.CSS_SELECTOR, 'input[data-zta="quantityStepperInput"]')
+        addproduct[producttype].clear()
+        addproduct[producttype].send_keys(productquantity)
+        elements = self.driver.find_elements(By.CSS_SELECTOR, 'button[data-zta="add-to-cart-button"]')
+        elements[producttype].click()
---
+++ test-zooplus/Test_zooplus.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from Data import Data
+import time
+import pytest
+
+# number of product in the order displayed on the page
+productnumber = 1
+# type of product in the order displayed on the page
+producttype = 0
+# quantity of the product to be added to the cart
+productquantity = 3
+
+driver = None
+core = None
+
+@pytest.fixture(scope="session", autouse=True)
+def setup():
+    global driver, core
+    driver = webdriver.Edge()
+    core = Data(driver)
+    yield
+    driver.quit()
+
+def test_accept_cookies():
+
+    core.open()
+    time.sleep(2)
+    core.click_cookies()
+    core.make_screenshot()
+
+
+def test_searching():
+
+    core.enter_product()
+    time.sleep(2)
+    core.make_screenshot()
+
+def test_adding_product_to_cart_1():
+
+    core.choosing_product(productnumber)
+    time.sleep(1)
+    core.make_screenshot()
+    core.add_product(producttype, productquantity)
+    core.make_screenshot()
+
+def test_deleting_product_from_cart():
+
+    core.enter_to_cart()
+    time.sleep(3)
+    core.make_screenshot()
+    core.delete_product(productquantity)
+    time.sleep(5)
+    core.make_screenshot()
+
+
+if __name__ == '__main__':
+    pytest.main(['-v'])
